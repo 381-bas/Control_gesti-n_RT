6f8d835c0feda50d0e134fbf46aa2a0d60760e5d
381-bas <bastian.baaz@gmail.com>
2026-06-15 17:20:16 -0400

    refactor(streamlit): activar acceso de datos por servicio V2

--- streamlit_app/data_access.py
+++ streamlit_app/data_access.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-"""Compatibilidad: la implementación vigente está en data_access_v1."""
+"""Compatibilidad: la implementación vigente está en data_access_v2."""
 
-from data_access_v1 import *  # noqa: F401,F403
+from data_access_v2 import *  # noqa: F401,F403
