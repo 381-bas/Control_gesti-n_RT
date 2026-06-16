fe8f05b1f8c1fad22183241cac8f1c04b7b52737
381-bas <bastian.baaz@gmail.com>
2026-06-16 12:16:59 -0400

    refactor(streamlit): activar semántica PITUTO V2.1

--- streamlit_app/data_access.py
+++ streamlit_app/data_access.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-"""Compatibilidad: la implementación vigente está en data_access_v2."""
+"""Compatibilidad: la implementación vigente está en data_access_v2_1."""
 
-from data_access_v2 import *  # noqa: F401,F403
+from data_access_v2_1 import *  # noqa: F401,F403
